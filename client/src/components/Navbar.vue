05aba94123c5a7e3e387ccc1253369505e0dca13
Jonathan Alphabert <jonathanalphabert0504@gmail.com>
2023-07-13 22:42:06 +0700

    Add navbar component

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav>
+    <div class="shop-name">
+      <h2>Toko Tunas Jaya</h2>
+    </div>
+    <div class="user-info">
+      <h4>Admin</h4>
+    </div>
+  </nav>
+</template>
+
+<style>
+nav {
+  background-color: var(--vt-c-black-soft);
+  color: var(--vt-c-white-soft);
+  padding: 0.5rem 3rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
